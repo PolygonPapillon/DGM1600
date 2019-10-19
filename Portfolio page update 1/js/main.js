860a0ea8a121904371bd30b05164e53dccf2e7a2
Heather <heegraphicgirl@gmail.com>
2019-10-19 15:40:20 -0600

    starting stuff

---
+++ Portfolio page update 1/js/main.js
@@ -0,0 +1,7 @@
+import { films } from './films.js'
+import { people } from './people.js'
+import { planets } from './planets.js'
+import { species } from './species.js'
+import { starships } from './starships.js'
+import { vehicles } from './vehicles.js'
+
