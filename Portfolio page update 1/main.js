c97625f342b0b916cf55722143da8b6ea8c0097c
Heather <heegraphicgirl@gmail.com>
2019-10-24 23:59:52 -0600

    still not quite fixed but I've got to turn it in...

--- Portfolio page update 1/main.js
+++ Portfolio page update 1/main.js
@@ -7,7 +7,44 @@ import { vehicles } from './sourceJS/vehicles.js'
 
 let mainArea = document.querySelector("main")
 
-films.forEach(function(film) {
+/* function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initate() {
+    var style1 = document.getElementById("stylesheet1");
+    var style2 = document.getElementById("stylesheet2");
+
+    style1.onclick = swapStyleSheet("default.css");
+    style2.onclick = swapStyleSheet("dark.css");
+} */
+
+/* function createButton() {
+    var element = document.createElement("button");
+    element.appendChild(document.createTextNode("Click Me!"));
+    var page = document.getElementById("btn");
+    page.appendChild(element);
+  
+    console.log(element);
+  }
+  
+  //createButton();
+
+var toggleTheme = function(themeName) {
+    var stylesheets = document.styleSheets;
+    var length = stylesheets.length;
+    var i;
+  
+    for(i = 0; i < length; i++) {
+      var ss = stylesheet[i];
+      ss.disabled = (ss.href.indexOf(themeName) !== -1) ? false : true;
+    }
+  }; */
+
+//mainArea.appendChild(styleToggle)
+
+
+/* films.forEach(function(film) {
     let filmDiv =document.createElement("div")
     let filmTitle =document.createElement("h1")
     let filmCrawl =document.createElement("p")
@@ -19,23 +56,27 @@ films.forEach(function(film) {
     filmDiv.appendChild(filmCrawl)
 
     mainArea.appendChild(filmDiv)
-});
+}); */
 
 people.forEach(function(person) {
     let personDiv = document.createElement("div")
     let name = document.createElement("h1")
-    let gender = document.createElement("h3")
+    //let homeworld = document.createElement("h3")
     let pic = document.createElement("img")
 
     personDiv.appendChild(name)
-    personDiv.appendChild(gender)
+    //personDiv.appendChild(homeworld)
     personDiv.appendChild(pic)
 
-    let charNum = getCharNumber(person.url)
+    
 
     name.textContent = person.name
-    gender.textContent = person.gender
-    pic.src = 'https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg'
+
+    //let webNum = getWebNumber(person.homeworld)
+    //homeworld.textContent = person.homeworld
+
+    let webNum = getWebNumber(person.url)
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${webNum}.jpg`
  
 
 
@@ -44,12 +85,13 @@ people.forEach(function(person) {
 
 });
 
-function getCharNumber(charURL) {
-    let end = charURL.lastIndexOf('/')
-    let charID = charURL.substring(end - 2, end)
-    if (charID.indexOf('/') !== -1 ) {
-        return charID.slice(1,2)
+function getWebNumber(webURL) {
+    let end = webURL.lastIndexOf('/')
+    let webID = webURL.substring(end -2, end)
+    if (webID.indexOf('/') !== -1 ) {
+        return webID.slice(1,2)
     } else {
-        return charID
+        console.log(webID)
+        return webID
     }
 }
