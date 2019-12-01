93b2060dc23129d8a45c9d1606de9723d6bc5c83
Heather <heegraphicgirl@gmail.com>
2019-12-01 00:24:33 -0700

    finally got the cards filled

--- flipCards_added/pokeJS.js
+++ flipCards_added/pokeJS.js
@@ -1,107 +1,108 @@
-var card = document.querySelector(".card")
-card.addEventListener("click", function() {
-  card.classList.toggle("is-flipped")
-})
+let requestURL =
+  "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  let pokedata = request.response;
+  console.log(pokedata);
+  populateDOM(pokedata);
+};
 
-//great, compact function for fetching json!!!
-async function getAPIData(url) {
-  try {
-    const response = await fetch(url) //fetch only gets urls!
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error(error)
+let mainArea = document.querySelector("main");
+
+function populateDOM(allPokemon) {
+  for (let i = 0; i < 30; i++) {
+
+    let pokeScene = document.createElement("section");
+    let pokeCard = document.createElement("article");
+    let pokeFront = document.createElement("div");
+    let pokeBack = document.createElement("div");
+
+    pokeScene.setAttribute("class", "scene");
+    pokeCard.setAttribute("class", "card");
+    pokeFront.setAttribute("class", "card__face card__face--front");
+    pokeBack.setAttribute("class", "card__face card__face--back");
+
+    makeCardFront(pokeFront, allPokemon, i);
+    makeCardBack(pokeBack, allPokemon, i);
+
+    pokeCard.appendChild(pokeFront);
+    pokeCard.appendChild(pokeBack);
+
+    pokeScene.appendChild(pokeCard);
+
+    mainArea.appendChild(pokeScene);
+
+    pokeCard.addEventListener("click", function() {
+      pokeCard.classList.toggle("is-flipped");
+    });
   }
 }
 
-const theData = getAPIData("https://pokeapi.co/api/v2/pokemon/").then(data => {
-  for (const pokemon of data.results) {
-    getAPIData(pokemon.url).then(pokedata => {
-      populateDOM(pokedata)
-    })
+function makeCardFront(pokeFront, aPokemon, arrayCount) {
+  let pokePic = document.createElement("img");
+  let pokeName = document.createElement("h1");
+  let pokeHP = document.createElement("p");
+
+  pokeName.textContent = aPokemon[arrayCount].name.english;
+  pokeHP.textContent = "HP for this Pokemon is: " + aPokemon[arrayCount].base.HP;
+
+  let pokeNum = getPokePic(aPokemon[arrayCount].id);
+
+  pokePic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeNum}.png`;
+
+  pokeFront.appendChild(pokePic);
+  pokeFront.appendChild(pokeName);
+  pokeFront.appendChild(pokeHP);
+}
+
+function makeCardBack(pokeBack, aPokemon, arrayCount) {
+  let pokeSprite = document.createElement("img");
+  let pokeType = document.createElement("h2");
+  let typeList = document.createElement("ul");
+  let otherNames = document.createElement("h2");
+  let frenchName = document.createElement("p");
+  let japaneseName = document.createElement("p");
+
+  // pokemon sprite
+  let pokeNum = getPokePic(aPokemon[arrayCount].id);
+  pokeSprite.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/sprites/${pokeNum}MS.png`;
+  pokeBack.appendChild(pokeSprite);
+
+  pokeType.textContent = "Type(s):";
+  pokeBack.appendChild(pokeType);
+
+  //typeList.textContent = aPokemon[arrayCount].type;
+  
+  
+  // listing the type(s) of each pokemon
+  for (let j = 0; j < aPokemon[arrayCount].type.length; j++) {
+    let listItem = document.createElement("li")
+    listItem.textContent = aPokemon[arrayCount].type[j]
+    typeList.appendChild(listItem)
   }
-})
+  pokeBack.appendChild(typeList);
 
-let mainArea = document.querySelector("main")
+  otherNames.textContent = aPokemon[arrayCount].name.english + "'s name in:";
+  frenchName.textContent = "French is:     " + aPokemon[arrayCount].name.french;
+  japaneseName.textContent =
+    "Japanese is:     " + aPokemon[arrayCount].name.japanese;
 
-function populateDOM(eachPokemon) {
-  let pokeScene = document.createElement("div")
-  let pokeCard = document.createElement("div")
-  let pokeFront = document.createElement("div")
-  let pokeBack = document.createElement("div")
-
-  pokeScene.setAttribute("class", "scene")
-  pokeCard.setAttribute("class", "card")
-  
-  makeCardFront(pokeFront, eachPokemon)
-  makeCardBack(pokeBack, eachPokemon)
-
-  pokeCard.appendChild(pokeFront)
-  pokeCard.appendChild(pokeBack)
-  pokeScene.appendChild(pokeCard)
-
-
-
-  mainArea.appendChild(pokeCard)
-
-  pokeCard.addEventListener("click", function() {
-    pokeCard.classList.toggle("is-flipped")
-  })
-
+  pokeBack.appendChild(otherNames);
+  pokeBack.appendChild(frenchName);
+  pokeBack.appendChild(japaneseName);
 }
-
-
-
-
-function makeCardFront(pokeFront, eachPokemon) {
-
-  pokeFront.getElementsByClassName("card__face--front")
-  
-  let pokePic = document.createElement("img")
-  let pokeName = document.createElement("h1")
-  let pokeType = document.createElement("p")
-
-
-
-  pokeName.textContent = eachPokemon.name
-  pokeType.textContent = eachPokemon.type
-
-  let pokeNum = getPokePic(eachPokemon.id)
-
-  pokePic.src = "../pokemon_json_files/images/${pokeNum}.png"
-
-  pokeFront.appendChild(pokePic)
-  pokeFront.appendChild(pokeName)
-  pokeFront.appendChild(pokeType)
-
-}
-
-
-function makeCardBack(pokeBack, eachPokemon) {
-  pokeBack = document.getElementsByClassName("card__face--back")
-
-  let pokeSprite = document.createElement("img")
-
-  let pokeNum = getPokePic(eachPokemon.id)
-
-  pokeSprite.src = "../pokemon_json_files/sprites/${pokeNum}MS.png"
-  
-  pokeBack.appendChild(pokeSprite)
-
-}
-
 
 function getPokePic(pokeID) {
   if (pokeID < 1 || pokeID > 964) {
-    return "Not a valid pokemon number!"
+    return "Not a valid pokemon number!";
   } else if (pokeID > 0 && pokeID < 10) {
-    return "00${pokeID}"
-  } else if (pokeID > 9 && pokeID > 100) {
-    return "0${pokeID}"
+    return `00${pokeID}`;
+  } else if (pokeID > 9 && pokeID < 100) {
+    return `0${pokeID}`;
   } else {
-    return pokeID
+    return pokeID;
   }
 }
-
-
-document.write(5 + 6)
